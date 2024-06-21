4f00f13d93be565920307b63c27699be9baa792c
Niteesh.bv <niteesh.bv@gmail.com>
2024-06-21 06:57:09 +0530

    Initial commit 22

---
+++ validation.js
@@ -0,0 +1,7 @@
+function validation(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var data = document.getElementById('data').value;
+    
+    return name,email,data;
+}
