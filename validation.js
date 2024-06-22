ab938b24a2081ff333616773cf12f2d68cd93fcc
Niteesh.bv <niteesh.bv@gmail.com>
2024-06-22 21:03:32 +0530

    Initial commit 24

--- validation.js
+++ validation.js
@@ -1,7 +1,20 @@
+function printErr(eleClass,message){
+    document.getElementsByClassName(eleClass)[0].innerHTML = message
+}
 function validation(){
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var data = document.getElementById('data').value;
     
-    return name,email,data;
+    if (name == ""){
+        console.log("yes")
+        printErr("nameid","Please enter your name");
+    }
+    else if (email == "") {
+        printErr("emailid","Please enter your email");
+    } 
+    else if (data == "Select") {
+        printErr("dataid","Please enter your Country");
+    }
+    return false
 }
